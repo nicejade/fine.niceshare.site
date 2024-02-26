e53651eb793076c7f42896a8787fd8574bda2da9
杨琼璞 <jeffygisgreat@gmail.com>
2024-02-27 00:42:29 +0800

    :chart_with_upwards_trend: inject GA4 for analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,20 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: '缘知随心庭',
+			head: [
+        {
+          tag: 'script',
+          attrs: {
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-7NRFYFR8BE',
+						'id': 'G-7NRFYFR8BE',
+            defer: true,
+					},
+				},
+				{
+          tag: 'script',
+					content: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7NRFYFR8BE');"
+        },
+      ],
 			social: {
 				github: 'https://github.com/nicejade/fine.niceshare.site',
 			},
