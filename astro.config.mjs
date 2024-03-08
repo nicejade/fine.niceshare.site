08daf46792832277241c94824a62e0acf6b6608b
杨琼璞 <jeffygisgreat@gmail.com>
2024-03-08 14:08:53 +0800

    :heavy_plus_sign: inject tailwind css & svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,82 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import starlightLinksValidator from 'starlight-links-validator'
 
 // https://astro.build/config
 export default defineConfig({
+	server: {
+		port: 6969,
+		host: true,
+	},
+	site: 'https://fine.niceshare.site/',
 	integrations: [
+		svelte(),
 		starlight({
+			plugins: [starlightLinksValidator()],
 			title: '缘知随心庭',
+			social: {
+				github: 'https://github.com/nicejade/fine.niceshare.site',
+				mastodon: 'https://mastodon.social/@nicejade',
+				'x.com': 'https://x.com/nicejadeyang',
+			},
+			logo: {
+				src: './src/assets/images/logo.svg',
+			},
+			lastUpdated: true,
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
+			customCss: [
+				// 你的自定义 CSS 文件的相对路径
+				'./src/assets/styles/custom.css',
+				'./src/assets/styles/tailwind.css',
+			],
 			head: [
-        {
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'apple-touch-icon',
+						sizes: '180x180',
+						href: '/apple-touch-icon.png',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						sizes: '32x32',
+						href: '/favicon-32x32.png',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'icon',
+						sizes: '16x16',
+						href: '/favicon-16x16.png',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						manifest: '/site.webmanifest',
+						re: 'manifest',
+					},
+				},
+				{
+					tag: 'meta',
+					attrs: {
+						name: 'theme-color',
+						content: '#ffffff',
+					},
+				},
+				{
           tag: 'script',
           attrs: {
             src: 'https://www.googletagmanager.com/gtag/js?id=G-7NRFYFR8BE',
@@ -19,10 +88,7 @@ export default defineConfig({
           tag: 'script',
 					content: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7NRFYFR8BE');"
         },
-      ],
-			social: {
-				github: 'https://github.com/nicejade/fine.niceshare.site',
-			},
+			],
 			sidebar: [
 				{
 					label: 'Guides',
@@ -37,5 +103,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		tailwind(),
 	],
-});
+})
